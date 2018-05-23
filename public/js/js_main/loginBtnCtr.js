925589a134a6ae9167fafc1400d8b89b4a4c195d
Junho Oh <joh29@my.bcit.ca>
2018-05-23 14:11:28 -0700

    added logout feature in every pages navbar, and fixed routing

--- public/js/js_main/loginBtnCtr.js
+++ public/js/js_main/loginBtnCtr.js
@@ -1,15 +1,24 @@
 // JUNHO
-function gameBtnControl(gameBtn){
-    var link = (gameBtn === 'game1Btn') ? '/game1' : '/game2';
-    var cur = document.getElementById("loVerf").innerHTML;
-    console.log(cur);
-    $('#modalOk').click(function(){
-      location.href=link;
-    })
-    if (cur === 'Guest'){
-      console.log('notlogged in');
-      $('#gameOption').modal({backdrop:true, focus:true});
-    } else{
-      location.href=link;
-    }
+function gameBtnControl(gameBtn) {
+  var link = (gameBtn === 'game1Btn') ? '/game1' : '/game2';
+  var cur = document.getElementById("loVerf").innerHTML;
+  console.log(cur);
+  $('#modalOk').click(function () {
+    location.href = link;
+  })
+  if (cur === 'Guest') {
+    console.log('notlogged in');
+    $('#gameOption').modal({ backdrop: true, focus: true });
+  } else {
+    location.href = link;
   }
+}
+
+var curStat = document.getElementById("grtVerf").innerHTML;
+document.getElementById("navLogout").innerHTML = '';
+if (curStat != '') {
+  document.getElementById("navLogout").innerHTML = 'Logout';
+  $('#navLogout').click(() => {
+    location.href = '/logout';
+  })
+}
